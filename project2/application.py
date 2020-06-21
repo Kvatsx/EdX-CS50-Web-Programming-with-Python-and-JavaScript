040f08f2de61de304232034f43d67227ceae5d97
kaustavvats <kaustav16048@iiitd.ac.in>
2020-06-22 04:28:36 +0530

    started project2

---
+++ project2/application.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('displayName')
+        print(name)
+        return redirect(url_for('home'))
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html', name="Kv")
+
